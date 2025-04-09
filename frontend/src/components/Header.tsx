729617a96831dffeab1dd6f2333ac0680d085630
LeaAKOSSETAN <tayeakossetan@gmail.com>
2025-04-09 14:29:42 +0100

    [lea]component header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-dark text-white py-3">
+      <div className="container">
+        <div className="d-flex justify-content-between">
+          <h1>Gestion des Stocks</h1>
+          <nav>
+            <ul className="nav">
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/">
+                  Accueil
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/history">
+                  Historique
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/categories">
+                  Catégories
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/ProduitTable">
+                ProduitTable
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
